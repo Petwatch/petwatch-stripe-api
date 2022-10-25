06b931a22dbb477f2e16823744e23ba282e243f6
Roman Fedoruk <rfedoruk@students.kennesaw.edu>
2022-10-25 18:59:58 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        STRIPE_API_SECRET: process.env.STRIPE_API_SECRET,
+        NEXT_PUBLIC_STRIPE_API_PUBLIC: process.env.NEXT_PUBLIC_STRIPE_API_PUBLIC,
+        NEXT_PUBLIC_APP_SCHEME: process.env.NEXT_PUBLIC_APP_SCHEME,
+        NEXT_PUBLIC_HOST:process.env.NEXT_PUBLIC_HOST,
+        STRIPE_APP_FEE:process.env.STRIPE_APP_FEE
+    }
+}
