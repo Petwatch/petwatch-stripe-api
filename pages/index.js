1c8b4c39d691acb726a925ceb58aa9ccc4f6ad23
Tatton Cameron <tattonzeta@gmail.com>
2022-11-11 17:26:08 -0500

    Co-authored-by: rfedoruk <rfedoruk@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Link from "next/link"
 import styles from "../styles/Home.module.css"
 
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Listen for any change on document `marie` in collection `users`
+exports.myFunctionName = functions.firestore
+    .document('building-codes/123456789/posts').onWrite((change, context) => {
+      // ... Your code here
+    });
+
 export default function Home() {
   return (
     <div className="container">
