d6ff1663f038d108a8c973c378a7ce5d02d5b183
Roman Fedoruk <rfedoruk@students.kennesaw.edu>
2022-11-26 21:05:06 -0500

    Update index.js
    removed unneeded code

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@ import Link from "next/link"
 import styles from "../styles/Home.module.css"
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
 // Listen for any change on document `marie` in collection `users`
-exports.myFunctionName = functions.firestore
-    .document('building-codes/123456789/posts').onWrite((change, context) => {
-      // ... Your code here
-  });
+// exports.myFunctionName = functions.firestore
+//     .document('building-codes/123456789/posts').onWrite((change, context) => {
+//       // ... Your code here
+//   });
 
 export default function Home() {
   return (
